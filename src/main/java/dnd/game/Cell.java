c09592789b366bcf39bbd2aad06caf7701b20266
Marie-Demeter <mdemeter.ducrot@gmail.com>
2022-05-09 11:51:56 +0200

    Refacto : instanciation nex event

--- src/main/java/dnd/game/Cell.java
+++ src/main/java/dnd/game/Cell.java
@@ -1,5 +1,14 @@
 package dnd.game;
 
+import dnd.character.enemies.Dragon;
+import dnd.character.enemies.Goblin;
+import dnd.character.enemies.Wizard;
+import dnd.equipement.heal.BigPotionLife;
+import dnd.equipement.heal.PotionLife;
+import dnd.equipement.spell.Fireball;
+import dnd.equipement.spell.Flash;
+import dnd.equipement.weapon.Club;
+import dnd.equipement.weapon.Sword;
 import dnd.event.Event;
 
 public class Cell {
@@ -15,11 +24,11 @@ public class Cell {
         String[] eventsTypes = {"dnd.equipement", "dnd.character.enemies"};
         int intRandom = (int) (Math.random() * (eventsTypes.length + 1));
         Event e = null;
-            try {
-                e = selectEvent(eventsTypes[intRandom]);
-            } catch (Exception ex) {
-                //System.out.println("Ce type de personnage n'existe pas."+ "\n");
-            }
+        try {
+            e = selectEvent(eventsTypes[intRandom]);
+        } catch (Exception ex) {
+            //System.out.println("Ce type de personnage n'existe pas."+ "\n");
+        }
         return e;
         /*
         String[] eventType = {"dnd.equipement.weapon.Club", "dnd.equipement.weapon.Sword", "dnd.equipement.spell.Flash", "dnd.equipement.spell.Fireball", "dnd.equipement.heal.PotionLife", "dnd.equipement.heal.BigPotionLife", "dnd.character.enemies.Dragon", "dnd.character.enemies.Goblin", "dnd.character.enemies.Wizard"};
@@ -36,8 +45,34 @@ public class Cell {
         }*/
     }
 
-
     private Event selectEvent(String eventType) {
+        int intRandom = (int) (Math.random() * 100);
+        Event e;
+        if (intRandom < 60) {
+            if (intRandom < 10) {
+                e = new Club();
+            } else if (intRandom < 20) {
+                e = new Sword();
+            } else if (intRandom < 30) {
+                e = new Flash();
+            } else if (intRandom < 40) {
+                e = new Fireball();
+            } else if (intRandom < 50) {
+                e = new PotionLife();
+            } else {
+                e = new BigPotionLife();
+            }
+        } else {
+            if (intRandom < 70) {
+                e = new Dragon();
+            } else if (intRandom < 85) {
+                e = new Wizard();
+            } else {
+                e = new Goblin();
+            }
+        }
+        return e;
+        /*
         String[] eventEquipement = {".Club", ".Sword", ".Flash", ".Fireball", ".PotionLife", ".BigPotionLife"};
         String[] eventEnemy = {".Dragon", ".Goblin", ".Wizard"};
         int intRandom;
@@ -66,7 +101,9 @@ public class Cell {
                     //System.out.println("Ce type de personnage n'existe pas."+ "\n");
                 }
             }
-        } else /* == dnd.enemies */ {
+
+         // == dnd.enemies
+        } else {
             intRandom = (int) (Math.random() * eventEnemy.length);
             try {
                 Class eventClass = Class.forName(eventType + eventEnemy[intRandom]);
@@ -76,6 +113,7 @@ public class Cell {
             }
         }
         return e;
+        */
     }
 
 
